4317c4fe15e63c2c36667ec205ff5861b31f84c6
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-20 01:28:48 +0600

    html updated

--- app.js
+++ app.js
@@ -14,16 +14,13 @@ addForm.addEventListener('submit', e => {
 
     item.innerHTML = `
 
-    <div class="flex justify-between items-center border-2 p-3 mb-4 rounded-xl lg:w-[75%] mx-auto">
-                <div>
-                    <li class="text-xl"> ${i} . ${todo}</li>
+    <li class="text-xl flex justify-between items-center border-2 p-3 mb-4 rounded-xl lg:w-[75%] mx-auto"> 
 
-                </div>
+    <span class="block">${i} . ${todo}</span>
 
-                <div>
-                    <i class="fa-solid fa-trash block cursor-pointer"></i>
-                </div>
-            </div>
+    <i class="fa-solid fa-trash block cursor-pointer"></i>
+
+    </li>
 
     `;
 
